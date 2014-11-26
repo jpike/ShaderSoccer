d299d077134be908e460ad77aea0fa54f00f2b2b
jpike <jacob@jacobpike.com>
2014-11-26 08:40:33 -0500

    Finishing initial CPU AI (fixes #4).

--- Assets/Scripts/ComputerFieldPlayerController.cs
+++ Assets/Scripts/ComputerFieldPlayerController.cs
@@ -16,15 +16,32 @@ public class ComputerFieldPlayerController : MonoBehaviour
 
     /// <summary>
     /// The maximum vertical speed at which the field player moves.
-    /// Units are Unity units (meters) per second.
+    /// Units are Unity units (meters) per second.  The default
+    /// value is slightly higher than the player's vertical move
+    /// speed because it was observed that the the computer players
+    /// would otherwise fail to move quickly enough to the ball.
     /// </summary>
-    public float MaxVerticalMoveSpeedInMetersPerSecond = 5.0f;
+    public float MaxVerticalMoveSpeedInMetersPerSecond = 6.0f;
 
     /// <summary>
-    /// The minimum distance (in meters) between the ball and the
-    /// player before the computer AI will attempts to move.
+    /// The minimum vertical distance (in meters) between the ball and the
+    /// player before the computer AI will attempt to move.  The ball must
+    /// be at least this far away from the player (vertically) before
+    /// the AI will attempt to move.
     /// </summary>
-    public float MinBallToPlayerDistanceInMeters = 1.0f;
+    public float MinBallToPlayerVerticalDistanceInMeters = 0.5f;
+
+    /// <summary>
+    /// The maximum total distance (in meters) between the ball and the
+    /// player before the computer AI will attempt to move.  If the ball is
+    /// further away than this (on the 2D plane), then no movement will
+    /// occur.  The default is chosen to be slightly less than half the width
+    /// of the playing field so that the player only moves when the ball gets
+    /// approximately on the same half of the field that the attached player
+    /// is on.  This helps provide slightly different behavior for computer AI
+    /// players - otherwise, they would often both move synchronously.
+    /// </summary>
+    public float MaxBallToPlayerTotalDistanceInMeters = 7.0f;
     #endregion
 
     #region Private Fields
@@ -54,12 +71,27 @@ public class ComputerFieldPlayerController : MonoBehaviour
     /// </summary>
     public void MoveBasedOnAi()
     {
-        // MAKE SURE THE DISTANCE BETWEEN THE BALL AND PLAYER POSITION MEETS SOME MINIMUM THRESHOLD.
-        // This prevents a distracting jitter that occurs from the ball moving too frequently.
+        // MAKE SURE THE VERTICAL DISTANCE BETWEEN THE BALL AND PLAYER POSITION MEETS SOME MINIMUM THRESHOLD.
+        // This prevents a distracting jitter that occurs from the ball moving too frequently.  If this minimum
+        // threshold weren't met, the player may be moved for small variations in the ball position around
+        // the player's center position, resulting in undesirable up-and-down jittering.
         Vector3 ballPosition = m_ball.Bounds.center;
-        float ballToPlayerDistance = Mathf.Abs(ballPosition.y - transform.position.y);
-        bool minDistanceReachedBetweenBallAndPlayer = (ballToPlayerDistance >= MinBallToPlayerDistanceInMeters);
-        if (!minDistanceReachedBetweenBallAndPlayer)
+        float ballToPlayerVerticalDistance = Mathf.Abs(ballPosition.y - transform.position.y);
+        bool minVerticalDistanceReachedBetweenBallAndPlayer = (ballToPlayerVerticalDistance >= MinBallToPlayerVerticalDistanceInMeters);
+        if (!minVerticalDistanceReachedBetweenBallAndPlayer)
+        {
+            // Return early since no movement is needed.
+            return;
+        }
+
+        // MAKE SURE THE BALL IS CLOSE ENOUGH TO THE PLAYER.
+        // This helps prevent scenarios where two AI players would move in similar patterns
+        // due to both detecting the ball regardless of where it exists in the playing field.
+        Vector2 ballPosition2d = new Vector2(ballPosition.x, ballPosition.y);
+        Vector2 playerPosition2d = new Vector2(transform.position.x, transform.position.y);
+        float ballToPlayerDistance = Vector2.Distance(ballPosition2d, playerPosition2d);
+        bool ballTooFarAwayFromPlayer = (ballToPlayerDistance > MaxBallToPlayerTotalDistanceInMeters);
+        if (ballTooFarAwayFromPlayer)
         {
             // Return early since no movement is needed.
             return;
