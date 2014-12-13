ca3a5653922869ade9e64c3e28c5257243682bdc
jpike <jacob@jacobpike.com>
2014-12-13 16:22:22 -0500

    Adding initial title, credits, and game setup screens (fixes #6, fixes #7).

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Responsible for loading different scenes.
+/// </summary>
+public class SceneLoader : MonoBehaviour
+{
+    /// <summary>
+    /// Loads the scene with the specified name.
+    /// </summary>
+    /// <param name="sceneName">The name of the scene to load.</param>
+    public void LoadScene(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+}
